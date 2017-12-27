ee9ca51aead71395bdfffc4bf982eebd080d48b5
Monika Williams <monika.m.williams@gmail.com>
2017-12-27 13:03:26 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -3,38 +3,21 @@ import React from 'react';
 
 class Bomb extends React.Component {
   constructor(props) {
-<<<<<<< HEAD
     super();
 
     this.state = {
      secondsLeft : props.initialCount
-=======
-    super(props);
-
-    this.state = {
-     secondsLeft : `${props.initialCount}`
->>>>>>> ce6fae0656f0feec2557b3bf2a1ad87066141e97
     }
   }
 
   render() {
-<<<<<<< HEAD
+
      const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
     return (
 
        <div>
        {message}
-=======
-    return (
-
        <div>
-       if this.props.secondsLeft !=== 0
-       <h1> ${this.props.secondsLeft} seconds left before I go boom!</h1>
-       else {
-         <h1> Boom!</h1>
-       }
->>>>>>> ce6fae0656f0feec2557b3bf2a1ad87066141e97
-      </div>
     )
   }
 }
